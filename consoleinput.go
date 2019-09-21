92f00ba11b97bde2378165b9fc2df79caa93a2c9
Liberbell <liberbell@gmail.com>
2019-09-21 17:48:42 +0900

    consoleinput5

--- consoleinput.go
+++ consoleinput.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello World!\n")
-	var s string
-	fmt.Scanln(&s)
-	fmt.Println(s)
+	// fmt.Printf("Hello World!\n")
+	// var s string
+	// fmt.Scanln(&s)
+	// fmt.Println(s)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	str, _ := reader.ReadString('\n')
+	fmt.Println(str)
 }
