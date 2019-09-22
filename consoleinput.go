e232b7836e24301212441d45ff2293af9e22ceab
Liberbell <liberbell@gmail.com>
2019-09-22 16:53:52 +0900

    consoleinput1

--- consoleinput.go
+++ consoleinput.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -15,4 +16,13 @@ func main() {
 	fmt.Print("Enter text: ")
 	str, _ := reader.ReadString('\n')
 	fmt.Println(str)
+
+	fmt.Print("Enter a number: ")
+	str, _ = reader.ReadString('\n')
+	f, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println('Value of number: ', f)
+	}
 }
