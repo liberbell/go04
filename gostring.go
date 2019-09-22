4fa0d35271ab2f3c38f904f2aabd98e7114e67c6
Liberbell <liberbell@gmail.com>
2019-09-22 17:12:28 +0900

    gostring7

--- gostring.go
+++ gostring.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 	str1 := "An implicity typed string"
-	fmt.Println(str1)
+	// fmt.Println(str1)
 	fmt.Printf("str1: %v:%T\n", str1, str1)
+	str2 := "An explicity typed string"
+	fmt.Printf("str2: %v:%T\n", str2, str2)
 }
