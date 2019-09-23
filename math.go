bbe00557d94f86e151aa3de2ba41ec91e1c0b10c
Liberbell <liberbell@gmail.com>
2019-09-23 11:59:56 +0900

    math4

--- math.go
+++ math.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func main() {
 	i1, i2, i3 := 12, 45, 68
 	intsum := i1 + i2 + i3
 	fmt.Println("Integer sum: ", intsum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatsum := f1 + f2 + f3
+	fmt.Println("Float sum: ", floatsum)
+
+	var b1, b2, b3, bigsum big.Float
+	b1.SetFloat64(23.5)
+	b2.SetFloat64(65.2)
+	b3.SetFloat64(76.3)
+
+	bigsum.Add(&b1, &b2).Add(&bigsum, &b3)
 }
