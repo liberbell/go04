45c82bbc0cfeb85d7f36e9907c89de43a4bd1996
Liberbell <liberbell@gmail.com>
2019-09-23 16:29:32 +0900

    dateandtime5

--- dateandtime.go
+++ dateandtime.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	t := time.Date(2019, time.November, 10, 23, 0, 0, 0, time.UTC)
 	fmt.Printf("Go launched at %s\n", t)
+
+	now := time.Now()
+	fmt.Printf("The time now is %s\n", t)
 }
