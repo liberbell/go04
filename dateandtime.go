bd80c45d1c3bdd51e41139e7e6ffaa2f0f94434d
Liberbell <liberbell@gmail.com>
2019-09-23 16:25:27 +0900

    dateandtime3

--- dateandtime.go
+++ dateandtime.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
-	t := time.date(2019, time.November, 10, 23, 0, 0, 0)
+	t := time.date(2019, time.November, 10, 23, 0, 0, 0, time.UTC)
+	fmt.Printf(t)
 }
