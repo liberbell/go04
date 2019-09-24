7392c0ad10760c0015ab650160e453cc8d6f9f84
Liberbell <liberbell@gmail.com>
2019-09-25 07:43:31 +0900

    arrays5

--- arrays.go
+++ arrays.go
@@ -14,4 +14,7 @@ func main() {
 	var numbers = [5]int{5, 3, 1, 2, 4}
 	fmt.Println(numbers)
 	fmt.Println(numbers[2])
+
+	fmt.Println("The number is colors: ", len(colors))
+	fmt.Println("The number is numbers: ", len(numbers))
 }
