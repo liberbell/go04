390dbbf52ca24d89ef4a6a3c82bc1eebb13198fd
Liberbell <liberbell@gmail.com>
2019-09-28 21:47:34 +0900

    switch4

--- switch.go
+++ switch.go
@@ -10,4 +10,10 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(6) + 1
 	fmt.Println("Day", dow)
+
+	result := ""
+	switch dow {
+	case condition:
+		result := "It's Sunday!"
+	}
 }
