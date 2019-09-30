791d8d285d116137588e83ddae3a0a3c1778c088
Liberbell <liberbell@gmail.com>
2019-10-01 07:37:06 +0900

    libraries2

---
+++ libraries.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	n1, l1 := FullName("Zephod", "Beeblerox")
+	fmt.Printf("Fullname: %v, number of chars: %v", n1, l1)
+
+	n2, l2 := FullNameNakedReturn("Arthur", "Dent")
+	fmt.Printf("Fullname: %v, number of chars: %v", n2, l2)
+}
+
+func FullName(f, l string) (string, int) {
+	full := f + " " + l
+	length := len(full)
+	return full, length
+}
