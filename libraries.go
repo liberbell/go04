ba7feaf3ecce09d94250ff1f90133c7979388a98
Liberbell <liberbell@gmail.com>
2019-10-01 07:39:34 +0900

    libraries5

--- libraries.go
+++ libraries.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	n1, l1 := FullName("Zephod", "Beeblerox")
-	fmt.Printf("Fullname: %v, number of chars: %v", n1, l1)
+	fmt.Printf("Fullname: %v, number of chars: %v\n", n1, l1)
 
 	n2, l2 := FullNameNakedReturn("Arthur", "Dent")
-	fmt.Printf("Fullname: %v, number of chars: %v", n2, l2)
+	fmt.Printf("Fullname: %v, number of chars: %v\n", n2, l2)
 }
 
 func FullName(f, l string) (string, int) {
