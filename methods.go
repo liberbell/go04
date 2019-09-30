b1d43d8fa2db50ccc03be205480799b3d92ba2e6
Liberbell <liberbell@gmail.com>
2019-10-01 07:45:32 +0900

    methods2

---
+++ methods.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Dog struct {
+	Breed  string
+	Weight int
+	Sound  string
+}
+
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+func main() {
+	poodle := Dog{"Poodle", 37, "Woof"}
+	fmt.Println(poodle)
+}
