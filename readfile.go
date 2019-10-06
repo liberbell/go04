97242ecb2cb2eccf4ba8f2b607cafba4f27672c5
Liberbell <liberbell@gmail.com>
2019-10-07 07:39:35 +0900

    readfile2

--- readfile.go
+++ readfile.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+)
+
 func main() {
 	filename := "./hello.txt"
+
+	content, err := ioutil.ReadFile(filename)
+	checkError(err)
+
+	fmt.Println("Read from file: ", content)
 }
 
 func checkError(err error) {
