f6e4d04251fb37f84f6592071754ac14eb700a6e
Liberbell <liberbell@gmail.com>
2019-10-06 14:02:29 +0900

    writefile2

--- writefile.go
+++ writefile.go
@@ -5,8 +5,12 @@ import "os"
 func main() {
 	connect := "Hello from go."
 	file, err := os.Create("./fromstring.txt")
+  checkError(err)
+  defe file.Close()
 }
 
 func checkError() {
-
+  if err != nil {
+    panic(err)
+  }
 }
