0845a460b801968782863a6b27e88d83a49d4d66
Liberbell <liberbell@gmail.com>
2019-10-08 07:42:04 +0900

    parseJson2

---
+++ parseJson.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := "http://services.explorecalifornia.org/json/tours.php"
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Response type: %T\n", resp)
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	contents := string(bytes)
+	fmt.Print(contents)
+}
